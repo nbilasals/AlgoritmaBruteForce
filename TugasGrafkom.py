e35413afcf8f74383479723cdaae997418540ba0
raihandelaindi20 <95555204+raihandelaindi20@users.noreply.github.com>
2021-12-08 18:39:44 +0800

    Update TugasGrafkom.py

--- TugasGrafkom.py
+++ TugasGrafkom.py
@@ -1,8 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 x1=int(input('Masukkan x1 = '))
 x2=int(input('Masukkan x2 = '))
 y1=int(input('Masukkan y1 = '))
 y2=int(input('Masukkan y2 = '))
 
+xs = []
+ys = []
 if x1==x2 :
     y1=y1+1
     x1=x1
@@ -10,6 +15,8 @@ if x1==x2 :
     x2=x2
     print ('Titik pembentuk garis: ')
     print ('(',x1 ,',',y1,') dan(',x2 ,',',y2,')' ) 
+    xs.append(x1,x2)
+    ys.append(y1,y2)
 elif y1==y2 :
     x1=x1+1
     y1=y1
@@ -17,6 +24,8 @@ elif y1==y2 :
     y2=y2
     print ('Titik pembentuk garis: ')
     print ('(',x1 ,',',y1,') dan(',x2 ,',',y2,')' ) 
+    xs.append(x1,x2)
+    ys.append(y1,y2)
 else :
     m=(y2-y1)/(x2-x1)
     N=(x2-x1)+1
@@ -30,6 +39,11 @@ else :
         y= m*(x-x1)+y1
         ya=round(y)
         print ('(',x ,',',ya,')' )
-        x=x+1;
-
-        
+        xs.append(x)
+        ys.append(ya)
+        x=x+1
+np.array(xs)
+np.array(ys)
+plt.plot(xs,ys,'o')
+plt.grid()
+plt.show()
