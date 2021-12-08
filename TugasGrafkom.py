8c107def4024c73dc9d8ae0c6355766d6a1b9387
raihandelaindi20 <95555204+raihandelaindi20@users.noreply.github.com>
2021-12-08 22:14:48 +0800

    Grafik

--- TugasGrafkom.py
+++ TugasGrafkom.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 x1=int(input('Masukkan x1 = '))
 x2=int(input('Masukkan x2 = '))
 y1=int(input('Masukkan y1 = '))
@@ -5,25 +8,32 @@ y2=int(input('Masukkan y2 = '))
 
 x=x1
 y=y1
-if x1==x2 : 
+
+xs = []
+ys = []
+if x1==x2 :
     print('Titik-titik pembentuk garis: ')
     while y <= y2 : 
         print ('(',x ,',',y+1,')' )
         y=y+1
-        
- elif y1==y2 :
+        xs.append(x)
+        ys.append(y)
+
+elif y1==y2 :
     print('Titik-titik pembentuk garis: ')
     while x <= x2 : 
         print ('(',x+1 ,',',y,')' )
         x=x+1
-        
-  else :
+        xs.append(x)
+        ys.append(y)
+
+else :
     m=(y2-y1)/(x2-x1)
     N=(x2-x1)+1
     print ('m=(y2-y1)/(x2-x1)')
     print ('Nilai m adalah ( ',y2 ,'-' ,y1,')/(',x2,'-',x1,') = ',m)
     print ('N=(x2-x1)+1')
-    print ('Nilai N adalah ( ',x2,'-',x1, ') +1= ',N)
+    print ('Nilai N adalah ( ',x2,'-',x1, ') + 1= ',N)
     print ('Titik-titik pembentuk garis: ')
     i=0
     while i<N:
@@ -31,9 +41,11 @@ if x1==x2 :
         ya=round(y)
         print ('(',x ,',',ya,')' )
         i=i+1;
+        xs.append(x)
+        ys.append(ya)
         x=x+1;
-      
-      
-        
-        
- 
+np.array(xs)
+np.array(ys)
+plt.plot(xs,ys)
+plt.grid()
+plt.show()
